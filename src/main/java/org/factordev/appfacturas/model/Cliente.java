fe4e21ff15a2611f882fa31dc0a9f6051343b563
Juan Felipe Herrera Pinzón <77707590+FelipeHerreraPinzon@users.noreply.github.com>
2024-06-21 00:00:07 -0500

    atributos cliente y producto

--- src/main/java/org/factordev/appfacturas/model/Cliente.java
+++ src/main/java/org/factordev/appfacturas/model/Cliente.java
@@ -1,4 +1,23 @@
 package org.factordev.appfacturas.model;
 
 public class Cliente {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    private String rut;
 }
--- src/main/java/org/factordev/appfacturas/model/Producto.java
+++ src/main/java/org/factordev/appfacturas/model/Producto.java
@@ -1,4 +1,33 @@
 package org.factordev.appfacturas.model;
 
 public class Producto {
+    private  int codigo;
+    private String nombre;
+    private float precio;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
+
+
 }
